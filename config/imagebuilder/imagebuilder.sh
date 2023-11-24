e420107fe9858345d91ae56bfdfe25859c893505
cuangky <124901681+cuangky@users.noreply.github.com>
2023-11-24 19:14:47 +0700

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -117,9 +117,9 @@ adjust_settings() {
 custom_packages() {
     cd ${imagebuilder_path}
     echo -e "${STEPS} Start adding custom packages..."
-    chmod +x clash.sh
-    echo -e "${STEPS} Start adding clash core openclash..."
-    bash clash.sh
+    
+    
+    
     
     wget -P files/www/luci-static/resources/view/status/include https://raw.githubusercontent.com/kzer00/repo/main/aarch64_cortex-a53/29_port.js
     wget -P files/usr/share/rpcd/acl.d/ https://raw.githubusercontent.com/kzer00/repo/main/aarch64_cortex-a53/luci-mod-status-index.json
@@ -134,7 +134,30 @@ custom_packages() {
     sed -i 's/option check_signature/# option check_signature/g' repositories.conf
     # Download other luci-app-xxx
     # ......
+  echo -e "${STEPS} Start adding clash core openclash..."
 
+curl https://raw.githubusercontent.com/vernesong/OpenClash/core/master/core_version >version.txt
+versi=$(awk -F: 'NR==2 {print $1}' version.txt)
+echo "Start Clash Core Download !"
+echo "Current Path: $PWD"
+
+mkdir -p files/etc/openclash/core
+cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz
+tar -zxvf clash-linux-arm64.tar.gz
+mv clash clash.bak
+rm -rf *.tar.gz
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-$versi.gz
+gzip -d clash-*.gz && mv clash-linux-* clash_tun
+rm -rf *.gz
+#mv clash files/etc/openclash/core/clash_tun
+wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz
+tar -zxvf clash-linux-arm64.tar.gz && mv clash clash_meta
+#files/etc/openclash/core/clash_meta
+rm -rf *.gz
+mv clash.bak clash
+chmod +x *
+  
     sync && sleep 3
     echo -e "${INFO} [ packages ] directory status: $(ls packages -l 2>/dev/null)"
 }
